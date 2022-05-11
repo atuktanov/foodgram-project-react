8465596a6e96c56f47f86454cc020b3bc84a3334
atuktanov <atuktanov@gmail.com>
2022-05-12 00:03:36 +0300

    test

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY', default='!%jy4aj&t8qgw-dyz8s0at6&r1o!)%pnrq2qhtgap^u6%8mww+')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
