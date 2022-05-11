087fa63039d04a900720f8cfcfbb69fa3eb119ab
atuktanov <atuktanov@gmail.com>
2022-05-12 01:27:00 +0300

    Правки после ревью 1

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -11,7 +11,4 @@ router.register('recipes', RecipeViewSet)
 urlpatterns = (
     path('users/', include('users.urls')),
     path('auth/', include('djoser.urls.authtoken')),
-    # path('tags/', include('tags.urls')),
-    # path('ingredients/', include('ingredients.urls')),
-    # path('recipes/', include('recipes.urls')),
     path('', include(router.urls)))
