34baf532b943323dc7a07aef2023f2f5d8ad9c7b
atuktanov <atuktanov@gmail.com>
2022-05-12 21:28:55 +0300

    test

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -71,31 +71,10 @@ class RecipeSerializer(serializers.ModelSerializer):
             return False
         return Recipe.objects.filter(cart__user=user, id=obj.id).exists()
 
-    def validate_ingredients(self, value):
-        import logging
-        logging.error(value)
-        ingredients = value
-        if not ingredients:
-            raise serializers.ValidationError({
-                'ingredients': 'Нельзя создать рецепт без ингредиентов'})
-        ingredient_list = []
-        for ingredient_item in ingredients:
-            ingredient = get_object_or_404(
-                Ingredient, id=ingredient_item['id'])
-            if ingredient in ingredient_list:
-                raise serializers.ValidationError(
-                    'Нельзя дублировать ингредиенты')
-            ingredient_list.append(ingredient)
-            if int(ingredient_item['amount']) <= 0:
-                raise serializers.ValidationError(
-                    {'ingredients': (
-                        'Количество ингредиента должно быть больше 0')})
-        return value
-
-    # def validate(self, data):
+    # def validate_ingredients(self, value):
     #     import logging
-    #     logging.error(data)
-    #     ingredients = self.initial_data.get('ingredients')
+    #     logging.error(value)
+    #     ingredients = value
     #     if not ingredients:
     #         raise serializers.ValidationError({
     #             'ingredients': 'Нельзя создать рецепт без ингредиентов'})
@@ -111,8 +90,27 @@ class RecipeSerializer(serializers.ModelSerializer):
     #             raise serializers.ValidationError(
     #                 {'ingredients': (
     #                     'Количество ингредиента должно быть больше 0')})
-    #     data['ingredients'] = ingredients
-    #     return data
+    #     return value
+
+    def validate(self, data):
+        ingredients = self.initial_data.get('ingredients')
+        if not ingredients:
+            raise serializers.ValidationError({
+                'ingredients': 'Нельзя создать рецепт без ингредиентов'})
+        ingredient_list = []
+        for ingredient_item in ingredients:
+            ingredient = get_object_or_404(
+                Ingredient, id=ingredient_item['id'])
+            if ingredient in ingredient_list:
+                raise serializers.ValidationError(
+                    'Нельзя дублировать ингредиенты')
+            ingredient_list.append(ingredient)
+            if int(ingredient_item['amount']) <= 0:
+                raise serializers.ValidationError(
+                    {'ingredients': (
+                        'Количество ингредиента должно быть больше 0')})
+        data['ingredients'] = ingredients
+        return data
 
     def add_ingredients(self, ingredients, recipe):
         for ingredient in ingredients:
