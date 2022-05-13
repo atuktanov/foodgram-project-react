3874539727e925546f15436d31c8042988c6794a
atuktanov <atuktanov@gmail.com>
2022-05-13 13:47:39 +0300

    test

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -48,8 +48,7 @@ class RecipeSerializer(serializers.ModelSerializer):
     image = Base64ImageField()
     tags = TagSerializer(read_only=True, many=True)
     author = UserSerializer(read_only=True)
-    ingredients = IngredientAmountSerializer(
-        source='ingredientamount_set', many=True)
+    ingredients = IngredientAmountSerializer(many=True)
     # ingredients = IngredientAmountSerializer(
     #     source='ingredientamount_set', read_only=True, many=True)
 
