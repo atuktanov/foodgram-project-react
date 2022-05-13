7c0c16e505168b653561fa0c53e7b6c2b8d48727
atuktanov <atuktanov@gmail.com>
2022-05-13 22:46:23 +0300

    test

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -68,11 +68,12 @@ class RecipeSerializer(serializers.ModelSerializer):
             'cooking_time')
 
     def validate(self, data):
-        # import logging
+        import logging
         # logging.error(data)
         # logging.error(self.initial_data.get('ingredients'))
-        # logging.error(data.pop('ingredientamount_set'))  # !!!!
+        logging.error(data.get('ingredientamount_set'))  # !!!!
         # ingredients = self.initial_data.get('ingredients')
+        logging.error(self.initial_data.get('ingredients'))
         ingredients = data.pop('ingredientamount_set')
         if not ingredients:
             raise serializers.ValidationError({
